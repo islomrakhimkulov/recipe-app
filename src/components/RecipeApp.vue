138b940af0ea7720ea600aa07117f31da9a6edb9
islomrakhimkulov <islomraximqulov448@gmail.com>
2021-11-29 17:10:08 +0500

    appear all of ArrayList in fetch

--- src/components/RecipeApp.vue
+++ src/components/RecipeApp.vue
@@ -1,39 +1,41 @@
 <template>
-    <div class="container">
+    <div class="container py-5">
         <b-input-group class="mt-3" @click="recipeButton()">
             <b-form-input placeholder="Search"></b-form-input>
             <b-button class="danger">Search</b-button>
         </b-input-group>
         <h2 class="p-4 text-white text-center">Recipe App with Edamam API</h2>
-        <div class="row g-3">
-           <div class="col-6">
-                <div class="card">
-                    <img :src="this.test_data[2].recipe.image" class="card-img-top " alt="card-image">
+        <div class="row gy-3">
+           <div class="col-md-6 m-auto">
+                <div class="card mb-4" v-for="(info,index) in this.test_data" :key="index">
+                    <img :src="info.recipe.image" class="card-img-top " alt="card-image">
                     <div class="card-body">
-                        <h5 class="card-title">{{ this.test_data[2].recipe.label }}</h5>
+                        <div class="card-title h2 fw-bolder">{{ info.recipe.label }}</div>
+                        <span class="fw-bold h4">Ingredients:</span><br>
                         <ul>
-                            <li v-for="item in this.test_data[2].recipe.ingredientLines" :key="item">
-                                {{ item }}
+                            <li v-for="(ingredient, index) in info.recipe.ingredientLines" :key="index">
+                                {{ ingredient }}
                             </li>
                         </ul>
-                        <p class="ps-1">Calories: {{ this.test_data[2].recipe.calories.toFixed(2) }}</p>
+                        <p class="ps-1 h6">Calories: {{ info.recipe.calories.toFixed(2) }}</p>
                     </div>
                 </div>
             </div>
-            <div class="col-6">
+            <!-- <div class="col-md-6">
                 <div class="card">
-                    <img :src="this.test_data[0].recipe.image" class="card-img-top " alt="card-image">
+                    <img :src="this.test_data[1].recipe.image" class="card-img-top " alt="card-image">
                     <div class="card-body">
-                        <h5 class="card-title">{{ this.test_data[0].recipe.label }}</h5>
-                        <ul>
-                            <li v-for="item in this.test_data[0].recipe.ingredientLines" :key="item">
-                                {{ item }}
-                            </li>
-                        </ul>
-                        <p class="ps-1">Calories: {{ this.test_data[0].recipe.calories.toFixed(2) }}</p>
+                        <div class="card-title h4 fw-bolder">{{ this.test_data[1].recipe.label }}</div>
+                        <span class="fw-bold">Ingredients:</span><br>
+                            <ul>
+                                <li v-for="(ingredient, index) in this.test_data[1].recipe.ingredientLines" :key="index">
+                                    {{ ingredient }}
+                                </li>
+                            </ul>
+                        <p class="ps-1 h6">Calories: {{ this.test_data[1].recipe.calories.toFixed(2) }}</p>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
@@ -55,24 +57,22 @@ export default {
            const response = await fetch(`https://api.edamam.com/search?app_id=${ API_ID }&app_key=${ API_KEY }&q=chicken`)
            const data = await response.json();
            this.test_data = data.hits;
-        //    console.dir(data);
-              console.log(this.test_data);
-        //    this.test_data.forEach((value,index)=>{
-        //        console.log(value,index);
-        //    });
+            console.log(this.test_data);
 
-        console.log(this.test_data[0].recipe);
+            console.log(this.test_data[0]);
 
-        // console.log(this.test_data[0].recipe.label);
-        
-        // this.test_data.forEach((item,index) => {
-        //     console.log(item,index);
-        // });
+            // console.log(this.test_data[0].recipe.label);
+            console.log(this.test_data[0].recipe);
+
+            
+            // this.test_data.forEach((item,index) => {
+            //     console.log(item,index);
+            // });
 
 
-        // .then(response => response.json())
-        // .then(json_data => this.test_data = json_data.test_data);
-        // console.log(response);
+            // .then(response => response.json())
+            // .then(json_data => this.test_data = json_data.test_data);
+            // console.log(response);
         },
     },
     mounted(){
@@ -83,8 +83,11 @@ export default {
 
 <style>
 .container{
-  max-width: 600px !important;
+  max-width: 700px !important;
   width: 100%;
   margin: auto;
+}
+ul li {
+    list-style: none;
 }
 </style>
