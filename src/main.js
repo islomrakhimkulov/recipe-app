9d29f92c25df997f32ad71591cb6fc3fd8857339
islomrakhimkulov <islomraximqulov448@gmail.com>
2021-12-02 21:00:37 +0500

    aos added

--- src/main.js
+++ src/main.js
@@ -3,14 +3,19 @@ import App from './App.vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.config.productionTip = false;
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-Vue.config.productionTip = false
 
 new Vue({
+  created () {
+    AOS.init()
+  },
   render: h => h(App)
 }).$mount('#app')
